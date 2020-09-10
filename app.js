8abfba4c82b612cef43ea892b97ab5cbfe59d36a
ismoyo23 <ismoyorplb@gmail.com>
2020-09-10 11:38:06 +0700

    add redis

--- app.js
+++ app.js
@@ -4,20 +4,28 @@ require("dotenv").config();
 let BodyParser = require("body-parser");
 let cors = require("cors");
 let morgan = require("morgan");
+let redis = require("redis");
+let client = redis.createClient();
 app.use(morgan("dev"));
 
 let conn = require("./src/helper/mysql");
 
 app.use(BodyParser.urlencoded({ extended: true }));
 app.use(BodyParser.json());
+app.use((request, response, next) => {
+  request.redis = client;
+  next();
+});
 
-let jwt = require("jsonwebtoken");
 let path = require("path");
 
 app.use(express.static("./ser/upload/"));
 app.use(express.static("./src/upload/"));
 app.use("/src", express.static(path.join(__dirname + "/src")));
 let router = require("./src/index");
+const bodyParser = require("body-parser");
+const { request } = require("http");
+const { response } = require("./src/helper");
 
 // use cors
 app.use(cors());
@@ -29,7 +37,7 @@ conn.connect(function (error) {
   console.log("database hash connected");
 });
 
-app.listen(3000, function () {
+app.listen(3002, function () {
   console.log("server run on port 3000");
 });
 
